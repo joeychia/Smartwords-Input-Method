7ac8ccc869d0686d102873d8e8bbce62b8b696c5
Joey Jia <joeychia@outlook.com>
2025-12-07 17:48:28 -0800

    Initial working version for the main react app. and ios scarfolding.

---
+++ ios/SmartWords/SmartWordsTests/SmartWordsTests.swift
@@ -0,0 +1,17 @@
+//
+//  SmartWordsTests.swift
+//  SmartWordsTests
+//
+//  Created by Joey Jia on 12/7/25.
+//
+
+import Testing
+@testable import SmartWords
+
+struct SmartWordsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
